5e057fc12cb547130cb7e7ab2b140a50f3c7d743
Junha-Jang <andy.jang@kaist.ac.kr>
2025-02-12 17:15:28 +0900

    fix: modify channel of h2h layer

--- algorithms/diffusion_forcing/models/gru.py
+++ algorithms/diffusion_forcing/models/gru.py
@@ -27,7 +27,7 @@ class ConvRNNCell(nn.Module):
                              padding=self.padding,
                              bias=bias)
 
-        self.h2h = nn.Conv2d(in_channels=in_channels,
+        self.h2h = nn.Conv2d(in_channels=hidden_channels,
                              out_channels=hidden_channels * channel_multiplier,
                              kernel_size=self.kernel_size,
                              padding=self.padding,
